a86a90ec9c085f26367c78779a0d7eaefc38b76d
shreyasD-24 <161922587+shreyasD-24@users.noreply.github.com>
2024-04-30 20:35:22 +0530

    Updated cors origin in app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ if(process.env.NODE_ENV != "production"){
 
 const app = express();
 
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors({origin: "https://trugpt.netlify.app", credentials: true}));
 app.use(express.json()); 
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
